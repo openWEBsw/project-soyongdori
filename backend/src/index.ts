8a9dda8c622cbce823efc95ff5f8992c9713660c
Sangyeong-Choi <tkddud06@naver.com>
2026-05-25 00:13:14 +0900

    feat: update package.json and tsconfig.json for TypeScript configuration; add initial Express server setup in index.ts

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/test', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
