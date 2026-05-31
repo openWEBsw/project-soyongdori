442fdae7f5ad10df718d2410371a6d3de54bbecb
Sangyeong-Choi <tkddud06@naver.com>
2026-06-01 00:23:48 +0900

    feat: modify prisma client adapter for database connectivity

--- backend/src/prisma/client.ts
+++ backend/src/prisma/client.ts
@@ -3,22 +3,23 @@ import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import { PrismaClient } from "../generated/prisma/client.js";
 
 const globalForPrisma = global as unknown as {
-  prisma: PrismaClient; 
-}; 
+  prisma: PrismaClient;
+};
 
 const adapter = new PrismaMariaDb({
   host: process.env.DATABASE_HOST,
-  port: Number(process.env.DB_PORT) || 3307,  
+  port: Number(process.env.DB_PORT) || 3307,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
   connectionLimit: 5,
+  allowPublicKeyRetrieval: true,
 });
 
 const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    adapter, 
-  }); 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma; 
+    adapter,
+  });
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 export default prisma; 
