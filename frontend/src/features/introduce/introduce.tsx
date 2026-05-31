9e1fc4d3c863ca39a05c2628a90dd281871e703f
Sangyeong-Choi <tkddud06@naver.com>
2026-05-31 13:40:46 +0900

    feat: add svg icon to introduce page

--- frontend/src/features/introduce/introduce.tsx
+++ frontend/src/features/introduce/introduce.tsx
@@ -129,6 +129,9 @@ const Introduce: React.FC = () => {
         <div className="max-w-6xl mx-auto px-6 md:px-12 flex flex-col items-center">
           <button className="bg-btn-primary-bg text-btn-primary-text rounded-md flex items-center justify-center gap-2.5 h-12 px-8 text-sm md:text-base font-bold hover:opacity-95 transition-opacity shadow-sm cursor-pointer">
             소용돌이에 합류하기
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25" />
+            </svg>
           </button>
         </div>
       </section>
