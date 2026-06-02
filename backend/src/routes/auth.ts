af45ebd22907efe74b1973b48f12dc93ad11fa89
LeeInsu <118724642+happylife10201020@users.noreply.github.com>
2026-06-02 10:42:24 +0900

    feat: 로그아웃, 리프레시 라우트 추가 및 withCredentials 추가

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import { signup, login } from '../controllers/authController.js';
+import { signup, login, logout, refresh } from '../controllers/authController.js';
 
 const router = Router();
 
 router.post('/signup', signup);
 router.post('/login', login);
+router.post('/logout', logout);
+router.post('/refresh', refresh);
 
 export default router;
